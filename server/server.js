558409c5176bb5945561bac90713dbf936007f2b
thanhcong <thanhcong1506@gmail.com>
2023-04-19 21:42:30 +0700

    commit

--- server/server.js
+++ server/server.js
@@ -47,19 +47,23 @@ const app = express();
 //   // next();
 // });
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+//   })
+// );
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 //routes
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
