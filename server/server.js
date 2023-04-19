f29bd6d3176c169ce22c71e818ba7c1999f08778
thanhcong <thanhcong1506@gmail.com>
2023-04-19 16:48:52 +0700

    edit cors

--- server/server.js
+++ server/server.js
@@ -19,37 +19,32 @@ const app = express();
 //middelwares
 
 app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header(
+  //   "Access-Control-Allow-Origin",
+  //   "https://thanhcongadmin.netlify.app/"
+  // );
   // res.header(
   //   "Access-Control-Allow-Headers",
   //   "Origin, X-Requested-With, Content-Type, Accept"
   // );
   // next();
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://thanhcongadmin.netlify.app/"
-  );
+
+  const allowedOrigins = [
+    // "http://localhost:3000",
+    "https://thanhcongecommerce.netlify.app/",
+    // "https://thanhcongadmin.netlify.app/",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
-
-  // const allowedOrigins = [
-  //   "http://localhost:3000",
-  //   "https://thanhcongecommerce.netlify.app/",
-  // ];
-  // const origin = req.headers.origin;
-  // if (allowedOrigins.includes(origin)) {
-  //   res.setHeader("Access-Control-Allow-Origin", origin);
-  // }
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  // );
-  // res.header("Access-Control-Allow-credentials", true);
-  // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  // next();
 });
 
 app.use(
