3e7968fa5fd12bd88b0e0fba08c9ccfacd33d207
thanhcong <thanhcong1506@gmail.com>
2023-04-19 21:37:39 +0700

    commit

--- server/server.js
+++ server/server.js
@@ -18,34 +18,34 @@ const app = express();
 
 //middelwares
 
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://thanhcongecommerce.netlify.app/"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+// app.use(function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://thanhcongecommerce.netlify.app/"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
 
-  // const allowedOrigins = [
-  //   "http://localhost:3000",
-  //   "https://thanhcongecommerce.netlify.app/",
-  //   "https://thanhcongadmin.netlify.app/",
-  // ];
-  // const origin = req.headers.origin;
-  // if (allowedOrigins.includes(origin)) {
-  //   res.setHeader("Access-Control-Allow-Origin", origin);
-  // }
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  // );
-  // res.header("Access-Control-Allow-credentials", true);
-  // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  // next();
-});
+//   // const allowedOrigins = [
+//   //   "http://localhost:3000",
+//   //   "https://thanhcongecommerce.netlify.app/",
+//   //   "https://thanhcongadmin.netlify.app/",
+//   // ];
+//   // const origin = req.headers.origin;
+//   // if (allowedOrigins.includes(origin)) {
+//   //   res.setHeader("Access-Control-Allow-Origin", origin);
+//   // }
+//   // res.header(
+//   //   "Access-Control-Allow-Headers",
+//   //   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   // );
+//   // res.header("Access-Control-Allow-credentials", true);
+//   // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+//   // next();
+// });
 
 app.use(
   cors({
