3fc80d4d5c232f78ef16f751012b5ed5c3f82805
thanhcong <thanhcong1506@gmail.com>
2023-04-24 10:33:31 +0700

    commit

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 export const allowedOrigins = [
-  "http://localhost:3000",
+  // "http://localhost:3000",
   // "http://localhost:3001",
-  "https://thanhcongecommerce.onrender.com/",
+  // "https://thanhcongecommerce.onrender.com/",
   "https://thanhcongecommerce.netlify.app/",
 ];
